e5879c6439b74d204efb1bc25485c0371f331afe
chronichahaha-spec <chronichahaha@gmail.com>
2026-01-22 20:05:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def load_data_and_train():
         if col in df.columns:
             df[col] = df[col].fillna(df[col].mean())
     
-    feature_names = ['Sulfate', 'ph', 'Hardness', 'Chloramines', 'Solids',   
+    feature_names = ['ph', 'Hardness', 'Solids', 'Chloramines',  'Sulfate',  
                      'Conductivity', 'Organic_carbon', 'Trihalomethanes', 'Turbidity']
     
     X = df[feature_names].copy()
